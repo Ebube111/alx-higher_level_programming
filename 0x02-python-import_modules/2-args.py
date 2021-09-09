b07d8f9d0bb11bcfe0356679cc5f0a5fbf1c0c80
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-09-09 21:03:23 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == "__name__":
+if __name__ == "__main__":
     j = len(argv) - 1
 
     if j == 0:
         print("{:d} argument.".format(j))
     elif j == 1:
-        print("{:d} arguments:".format(j))
+        print("{:d} argument:".format(j))
     else:
         print("{:d} arguments:".format(j))
     for i in range(1, j + 1):
