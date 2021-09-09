192d3e45d7a4a8c887149b09ed1915c620d40369
Ebube Agwaze <ebubeagwaze@gmail.com>
2021-09-09 12:57:55 -0700

    import modules

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+l = dir(hidden_4)
+
+if __name__ == "__name__":
+    for i in range(0, len(1)):
+        if l[i][0] != "_" and l[i][1] != "_":
+            print(l[i])
