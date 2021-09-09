7314522d07a199efb7f4020cae977459dd6e2def
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-09-09 21:04:42 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ import hidden_4
 
 l = dir(hidden_4)
 
-if __name__ == "__name__":
-    for i in range(0, len(1)):
+if __name__ == "__main__":
+    for i in range(0, len(l)):
         if l[i][0] != "_" and l[i][1] != "_":
             print(l[i])
