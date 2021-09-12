e9f8856c23972696255bd80e3a30aa092a35bf8a
Ebube111 <ebubeagwaze@gmail.com>
2021-09-13 00:19:35 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = []
+def replace_in_list(my_list, idx, element):
     for i in my_list:
-        new_list = my_list[:]
         if idx < 0 or idx > len(my_list) - 1:
-            return new_list
-        for i in new_list:
-            new_list[idx] = element
-            return new_list
+            return my_list
+        else:
+            my_list[idx] = element
+            return my_list
