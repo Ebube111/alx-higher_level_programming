96335a4bd5643e06193ff3d1f731451e14cc6851
Ebube111 <ebubeagwaze@gmail.com>
2021-10-04 20:47:11 +0100

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,69 +1,56 @@
 #!/usr/bin/python3
 """
-Defines: class Rectangle
-Creates a Rectangle object height and width and calculates its area,
-perimeter and prints it with #
+a class Rectangle that defines a rectangle by:
+(based on 2-rectangle.py)
 """
 
 
 class Rectangle:
-    """Represents a Rectangle width and height
+    """
+    a class Rectangle
     """
     def __init__(self, width=0, height=0):
-        """Initializes a rectangle
-        """
-        self.__width = width
-        self.__height = height
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """Private instance attribute __width
-        """
         return self.__width
-
-    @property
-    def height(self):
-        """rectangle height
-        Returns private instance attribute __height
-        """
-        return self.__height
 
     @width.setter
     def width(self, value):
-        """set of rectangle width
-        """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        return self.__height
+
     @height.setter
     def height(self, value):
-
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """Returns the area of the rectangle
-        """
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
-        """Returns the perimeter of the rectangle
-        """
-        if self.__width == 0 or self.__height == 0:
+        if self.width is 0 or self.height is 0:
             return 0
-        return 2 * self.__width + 2 * self.__height
+        return (2 * self.width) + (2 * self.height)
 
     def __str__(self):
-        """Returns string
-        """
-        rep_str = ""
-        if self.__width != 0 and self.__height != 0:
-            rep_str += "\n".join("#" * self.__width
-                for i in range(self.__height))
-        return rep_str
+        result = ""
+        if not self.area():
+            return result
+        for i in range(self.height):
+            result += "#" * self.width
+            if i != self.height - 1:
+                result += "\n"
+        return result
