f0c5a1897908f1a02975497ad9f9d222da792df1
Ebube111 <ebubeagwaze@gmail.com>
2021-12-13 23:51:31 +0100

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-const array = process.argv.slice(2);
-if (array[0] === undefined && array[1] === undefined) {
-    console.log('undefined is undefined');
-} else if (array[0] !== undefined && array[1] !== undefined) {
-    console.log(array[0] + ' is ' + array[1]);
-} else if (array[0] !== undefined && array[1] === undefined) {
-    console.log(array[0] + ' is undefined');
-} else if (array[0] === undefined && array[1] !== undefined) {
-    console.log('undefined is ' + array[1]);
-}
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(arg1 + ' is ' + arg2);
