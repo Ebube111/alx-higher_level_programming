de0e8e7b35605a9a16e3201b67ee082be088e6f7
Ebube111 <ebubeagwaze@gmail.com>
2021-12-14 22:58:11 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,20 @@
 #!/usr/bin/node
-// a class Rectangle that defines a rectangle
 module.exports = class Rectangle {
-    constructor (w, h) {
-	// If w or h is equal to 0 or not a positive integer, create an empty object
-	if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
-	}
-    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-    // an instance method called print() that prints the rectangle using the character X
-    print () {
-	for (let i = 0; i < this.height; i++) {
-	    console.log('X'.repeat(this.width));
-	}
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
 
-    // an instance method called rotate() that exchanges the width and the height of the rectangle
-    rotate () {
-	this.width += this.height;
-	this.height = this.width - this.height;
-	this.width -= this.height;
-    }
+  rotate () {
+    this.width = this.height;
+    this.height = this.width;
+  }
 
-    // an instance method called double() that multiples the width and the height of the rectangle by 2
-    double () {
-	this.width *= 2;
-	this.height *= 2;
-    }
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 };
