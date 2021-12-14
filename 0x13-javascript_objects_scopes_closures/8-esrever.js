d72d661bbcb7fc957d241a06baceab76dde3d10e
Ebube111 <ebubeagwaze@gmail.com>
2021-12-14 23:04:37 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-// a function that returns the reversed version of a list
 exports.esrever = function (list) {
-    const rev = [];
-    list.forEach(elm => {
-	rev.unshift(elm);
-    });
-    return rev;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
