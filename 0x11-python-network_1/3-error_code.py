adc41609c05ee7afbf1525ab46d80be4a53900dc
Ebube111 <ebubeagwaze@gmail.com>
2022-01-09 23:33:14 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-# Python script that takes in a URL sends a request to the URL
+# Take in a URL, send a request to URL, and dispaly body
+
 import sys
 import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
-        with urllib.request.urlopen(sys.argv[1]) as res:
-                    print(res.info()['X-Request-Id'])
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as res:
+            print(res.read().decode('utf-8'))
+    except urllib.error.URLError as e:
+        print("Error code: {}".format(e.code))
