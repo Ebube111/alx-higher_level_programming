8dee76afbb85203e2ad0e1601a91bf0be9f89e07
Ebube111 <ebubeagwaze@gmail.com>
2022-01-09 23:34:09 +0100

    Update 4-htbn_header.py

--- 0x11-python-network_1/4-htbn_header.py
+++ 0x11-python-network_1/4-htbn_header.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-# Python script that takes in a URL sends a request to the URL
-import sys
-import urllib.request
+"""
+fetches https://intranet.hbtn.io/status
+"""
+import requests
 
-if __name__ == "__main__":
-        with urllib.request.urlopen(sys.argv[1]) as res:
-                    print(res.info()['X-Request-Id'])
+if __name__ == '__main__':
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
