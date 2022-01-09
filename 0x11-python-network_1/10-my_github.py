a05f2c938e7fe26105a29e261fd6e1de511a4d25
Ebube111 <ebubeagwaze@gmail.com>
2022-01-09 23:31:41 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-# Python script that takes in a URL sends a request to the URL
-import sys
-import urllib.request
+"""
+Python script takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
 
-if __name__ == "__main__":
-        with urllib.request.urlopen(sys.argv[1]) as res:
-                    print(res.info()['X-Request-Id'])
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
