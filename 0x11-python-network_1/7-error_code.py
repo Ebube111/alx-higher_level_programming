9af882e50e0e435f1c482bc32dfd420ee1a07ce9
Ebube111 <ebubeagwaze@gmail.com>
2022-01-09 23:38:59 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-# Python script that takes in a URL sends a request to the URL
-import sys
-import urllib.request
+"""
+Python Script takes in a URL, sends a request to the URL
+& displays the body of the response
+"""
+import requests
+from sys import argv
 
-if __name__ == "__main__":
-        with urllib.request.urlopen(sys.argv[1]) as res:
-                    print(res.info()['X-Request-Id'])
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    status = r.status_code
+    print(r.text) if status < 400 else print(
+        "Error code: {}".format(r.status_code))
