c482c8156bf0f8bc8a91dc3377364b6e3c0c586d
Ebube111 <ebubeagwaze@gmail.com>
2022-01-09 23:30:48 +0100

    Update 1-htbn_header.py

--- 0x11-python-network_1/1-htbn_header.py
+++ 0x11-python-network_1/1-htbn_header.py
@@ -4,5 +4,5 @@ import sys
 import urllib.request
 
 if __name__ == "__main__":
-        with urllib.request.urlopen(sys.argv[1]) as res:
-                    print(res.info()['X-Request-Id'])
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.info()['X-Request-Id'])
