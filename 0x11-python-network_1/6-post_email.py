8024845cf7879e997787e1d66f7ef165df4351f1
Ebube111 <ebubeagwaze@gmail.com>
2022-01-09 23:38:29 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-# Python script that takes in a URL sends a request to the URL
-import sys
-import urllib.request
+"""
+takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
+"""
+import requests
+from sys import argv
 
-if __name__ == "__main__":
-        with urllib.request.urlopen(sys.argv[1]) as res:
-                    print(res.info()['X-Request-Id'])
+if __name__ == '__main__':
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
