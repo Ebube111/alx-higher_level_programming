79ff0a39c5574866c5bb3c0036c8c2fe1fbac758
Ebube111 <ebubeagwaze@gmail.com>
2022-02-01 19:45:03 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,7 +6,7 @@ const file = process.argv[2];
 const write_ = process.argv[3];
 
 fs.writeFile(file, write_, 'utf8', function (error) {
-    if (error) {
-	console.log(error);
-    }
+  if (error) {
+    console.log(error);
+  }
 });
