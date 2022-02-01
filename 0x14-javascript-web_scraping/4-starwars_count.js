63d0d674d10be69613184884f79047fa77b5fa65
Ebube111 <ebubeagwaze@gmail.com>
2022-02-01 19:58:07 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,25 +1,17 @@
 #!/usr/bin/node
-// prints the number of movies where the character Wedge Antilles is present
-
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
-    let jso = JSON.parse(body);
-    let results = jso['results'];
+// a script that prints the number of movies where the character ''Wedge Antilles' is present
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) { console.log(err); } else if (res.statusCode === 200) {
+    const films = JSON.parse(body).results;
     let count = 0;
-    for (let i = 0; i < results.length; i++) {
-      let chars = (results[i]['characters']);
-      for (let j = 0; j < chars.length; j++) {
-        let check18 = chars[j].endsWith('18/');
-        if (check18) {
-          count++;
-        }
+    for (const idx in films) {
+      const filmChars = films[idx].characters;
+      for (const cidx in filmChars) {
+        if (filmChars[cidx].includes('18')) { count++; }
       }
     }
     console.log(count);
-  }
+  } else { console.log('An error occured. Status code: ' + res.statusCode); }
 });
